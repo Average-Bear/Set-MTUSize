28e6878edbaf28a10a04f25bdf0d890fee4263a8
Average-Bear <jeremyndewitt@gmail.com>
2017-11-01 23:45:59 +1200

    Update Set-MTUSize.ps1

--- Set-MTUSize.ps1
+++ Set-MTUSize.ps1
@@ -9,13 +9,16 @@
     Select local or remote computer by hostname or IP address.
 
 .PARAMETER NetworkAdapters
-    Select network interface card (NIC) by name, Default='Local Area Connection'.
+    Select network interface card (NIC) by name.
+    Default='Local Area Connection'
 
 .PARAMETER MTUSize
-    Set maximum MTU size; Default='1300'.
+    Set maximum MTU size.
+    Default='1500'
 
 .PARAMETER Store
-    Set configuration storage interval; Default='Persistent'.
+    Set configuration storage interval. 
+    Default='Persistent'
 
 .NOTES
     Author: JBear
@@ -25,15 +28,20 @@
 param(
 
     [Parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
     [String[]]$Computername,
 
     [Parameter(ValueFromPipeline=$true,HelpMessage="NIC Name")]
+    [ValidateNotNullOrEmpty()]
     [String[]]$NetworkAdapters='Local Area Connection',   
 
     [Parameter(ValueFromPipeline=$true)]
-    [String]$MTUSize='1300',
+    [ValidateNotNullOrEmpty()]
+    [Int]$MTUSize='1500',
 
     [Parameter(ValueFromPipeline=$true, HelpMessage="Active or Persistent")]
+    [ValidateNotNullOrEmpty()]
+    [ValidateSet("Persistent", "Active")]
     [String]$Store='Persistent'
 )
     
@@ -49,30 +57,20 @@ function Set-MTUValue {
             Write-Progress -Activity "Setting MTU to $($MTUSize)..." -Status ("Percent Complete:" + "{0:N0}" -f ((($i++) / $Computername.count) * 100) + "%") -CurrentOperation "Processing $($Computer)..." -PercentComplete ((($j++) / $Computername.count) * 100)
 
             if(Test-Connection -Quiet -Count 1 -Computer $Computer) {
-                
-                $Session = New-PSSession -ComputerName $Computer
+                        
+                Invoke-Command -ComputerName $Computer { param($NetworkAdapters, $MTUSize, $Store)
 
-                if($Session) {
-        
-                    Invoke-Command -ComputerName $Computer { param($NetworkAdapters, $MTUSize, $Store)
+                    foreach($NIC in $NetworkAdapters) {
 
-                        foreach($NIC in $NetworkAdapters) {
-
-                            $Before = netsh Interface IPv4 Show SubInterface
-                            netsh Interface IPv4 Set SubInterface $($NIC) MTU=$MTUSize Store=$Store | Out-Null
-                            $After = netsh Interface IPv4 Show SubInterface
+                        $Before = netsh Interface IPv4 Show SubInterface
+                        netsh Interface IPv4 Set SubInterface $($NIC) MTU=$MTUSize Store=$Store | Out-Null
+                        $After = netsh Interface IPv4 Show SubInterface
                             
-                            "`n" + $env:COMPUTERNAME
-                            $Before
-                            $After + "`n"                      
-                        }
-                    } -ArgumentList $NetworkAdapters, $MTUSize, $Store -AsJob -JobName "Set MTU: $($MTUSize)."
-                }
-
-                else {
-                
-                    Write-Host -ForegroundColor "[Notice] Unable to connect via WinRM - $($Computer)"
-                }
+                        "`n" + $env:COMPUTERNAME
+                        $Before
+                        $After + "`n"                      
+                    }
+                } -ArgumentList $NetworkAdapters, $MTUSize, $Store -AsJob -JobName "Set MTU: $($MTUSize)."
             }
 
             else {
@@ -85,8 +83,6 @@ function Set-MTUValue {
         
             Write-Host -ForegroundColor Red "[Notice] Value is null."
         }
-
-        Remove-PSSession $Session
     }
 }
 
